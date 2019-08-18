1530eac1da4e17208beac18061058e0fbef7ed19
SilvioGazeta <gazetasilvio@gmail.com>
2019-08-18 15:28:25 -0300

    Add files via upload

---
+++ ex001_ola_mundo.py
@@ -0,0 +1,19 @@
+print('Opção 1: Cadeia de Caracteres')
+print('Olá, Mundo!')
+print('') #pula uma linha
+print('Opção 2: Com uso de Variável')
+msg = 'Olá, Mundo!'
+print(msg)
+print('') #pula uma linha
+print('Opção 3: Com uso de Variável e Método')
+msg = 'Olá, mundo!'
+print('{}'.format(msg))
+print('') #pula uma linha
+print('Opção 3: Com uso de Variável e Método (versão moderna)')
+msg = 'Olá, Mundo!'
+print(f'{msg}')
+print('') #pula uma linha
+print('Opção 4: Com uso de Função')
+def msg():
+    print('Olá, Mundo')
+msg()
